72b01e65b5eef9c4b91c84cf0b42fda0215f0757
kike <kikeramos9@gmail.com>
2020-11-18 00:02:06 +0100

    added new features

---
+++ app/src/public/precache-manifest.3df0a4dc5e89561fc4dd738eb83cdc78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f78e61089a15d74df77b8b2b23f391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8e3b6834b5a96cf86d0",
+    "url": "/static/css/main.e30db574.chunk.css"
+  },
+  {
+    "revision": "eb729d09ad46df7674bb",
+    "url": "/static/js/2.bbfdcb0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbfdcb0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e3b6834b5a96cf86d0",
+    "url": "/static/js/main.2a97ccec.chunk.js"
+  },
+  {
+    "revision": "9048dd95493fb2095d5d",
+    "url": "/static/js/runtime-main.a69dbb19.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
